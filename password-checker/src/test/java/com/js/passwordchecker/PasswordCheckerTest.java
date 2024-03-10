22b801083c345e05f3adba9308dda6924c57ffcb
Jaster25 <js_328@naver.com>
2024-03-10 18:14:58 +0900

    [암호 검사기] 암호 강도: 보통 테스트 코드 작성

--- password-checker/src/test/java/com/js/passwordchecker/PasswordCheckerTest.java
+++ password-checker/src/test/java/com/js/passwordchecker/PasswordCheckerTest.java
@@ -7,6 +7,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class PasswordCheckerTest {
 
+    private PasswordChecker passwordChecker = new PasswordChecker();
+
     @DisplayName("인자가 null인 경우 예외 발생")
     @Test
     void nullInput() {
@@ -16,7 +18,7 @@ public class PasswordCheckerTest {
 
         // then
         assertThrows(IllegalArgumentException.class,
-                () -> new PasswordChecker().check(null));
+                () -> passwordChecker.check(null));
     }
 
     @DisplayName("인자가 빈 값인 경우 예외 발생")
@@ -28,18 +30,34 @@ public class PasswordCheckerTest {
 
         // then
         assertThrows(IllegalArgumentException.class,
-                () -> new PasswordChecker().check(""));
+                () -> passwordChecker.check(""));
     }
 
-    @DisplayName("모든 조건을 충족하면 암호 강도 강함")
+    @DisplayName("모든 조건 충족 -> 암호 강도: 강함")
     @Test
     void meetAllRules() {
         // given
 
         // when
-        PasswordStrength passwordStrength = new PasswordChecker().check("abcABC123");
+        PasswordStrength result1 = passwordChecker.check("abcABC123");
+        PasswordStrength result2 = passwordChecker.check("1ab2cA3BC");
 
         // then
-        assertEquals(PasswordStrength.STRONG, passwordStrength);
+        assertEquals(PasswordStrength.STRONG, result1);
+        assertEquals(PasswordStrength.STRONG, result2);
+    }
+
+    @DisplayName("길이가 8 미만, 다른 조건들은 충족 -> 암호 강도: 보통")
+    @Test
+    void meetDigitAndUppercaseRules() {
+        // given
+
+        // when
+        PasswordStrength result1 = passwordChecker.check("aBC123");
+        PasswordStrength result2 = passwordChecker.check("1B3C2");
+
+        // then
+        assertEquals(PasswordStrength.NORMAL, result1);
+        assertEquals(PasswordStrength.NORMAL, result2);
     }
 }
