500ee81f6dacc39ad5096fe6b66071e1bb427c29
Jaster25 <js_328@naver.com>
2024-03-12 23:46:55 +0900

    [멤버십 적립 서비스] Membership 서비스 - 멤버십 상세 조회 기능 구현

--- membership-api/src/test/java/com/js/membershipapi/MembershipService.java
+++ membership-api/src/test/java/com/js/membershipapi/MembershipService.java
@@ -23,7 +23,7 @@ public class MembershipService {
     }
 
     public Membership getMembership(Long membershipId) {
-        membershipRepository.findById(membershipId);
-        throw new IllegalArgumentException("존재하지 않는 멤버십입니다.");
+        return membershipRepository.findById(membershipId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 멤버십입니다."));
     }
 }
