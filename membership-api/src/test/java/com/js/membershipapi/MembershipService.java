d9f3d1e67dbb63775702412d019fc3de55bd5555
Jaster25 <js_328@naver.com>
2024-03-12 23:01:08 +0900

    [멤버십 적립 서비스] Membership 서비스 - 멤버십 목록 조회 테스트 코드 추가

--- membership-api/src/test/java/com/js/membershipapi/MembershipService.java
+++ membership-api/src/test/java/com/js/membershipapi/MembershipService.java
@@ -1,13 +1,16 @@
 package com.js.membershipapi;
 
+import com.js.membershipapi.domain.membership.entity.Membership;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class MembershipService {
 
-    public void getMemberships(Long memberId) {
+    public List<Membership> getMemberships(Long memberId) {
         throw new IllegalArgumentException("존재하지 않는 멤버입니다.");
     }
 }
--- membership-api/src/test/java/com/js/membershipapi/MembershipServiceTest.java
+++ membership-api/src/test/java/com/js/membershipapi/MembershipServiceTest.java
@@ -1,5 +1,8 @@
 package com.js.membershipapi;
 
+import com.js.membershipapi.domain.member.entity.Member;
+import com.js.membershipapi.domain.membership.entity.Membership;
+import com.js.membershipapi.domain.membership.entity.MembershipType;
 import com.js.membershipapi.domain.membership.repository.MembershipRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -10,6 +13,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(MockitoExtension.class)
@@ -33,7 +39,33 @@ public class MembershipServiceTest {
                 () -> membershipService.getMemberships(memberId));
     }
 
-    // TODO: 멤버십 목록 조회
+    @DisplayName("멤버십 목록 조회")
+    @Test
+    void getMemberships() {
+        // given
+        Long memberId = 123L;
+        Member member = Member.builder()
+                .id(memberId)
+                .name("김회원")
+                .build();
+        Membership membership1 = Membership.builder()
+                .member(member)
+                .membershipType(MembershipType.GSNPOINT)
+                .build();
+        Membership membership2 = Membership.builder()
+                .member(member)
+                .membershipType(MembershipType.KAKAO)
+                .build();
+
+        BDDMockito.given(membershipRepository.findAllByMemberId(memberId))
+                .willReturn(List.of(membership1, membership2));
+
+        // when
+        List<Membership> foundMemberships = membershipService.getMemberships(memberId);
+
+        // then
+        assertEquals(2, foundMemberships.size());
+    }
 
     // TODO: 멤버십 상세 조회
     // TODO: 멤버십 포인트 적립(적립 방식은 확장 가능하게)
